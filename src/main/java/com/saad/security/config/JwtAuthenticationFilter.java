98e8405e50dda64f030a567dae4c8bcc3c29df69
Saad Sayyed <saadsyed950@gmail.com>
2024-12-20 11:42:03 +0530

    Working on authentication using tokens

--- src/main/java/com/saad/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/saad/security/config/JwtAuthenticationFilter.java
@@ -14,10 +14,12 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private JwtService jwtService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -25,5 +27,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
