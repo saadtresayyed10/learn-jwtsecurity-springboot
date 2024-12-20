16e71d3dc677120cd6d5b0577c11cb3d1a136a6c
Saad Sayyed <saadsyed950@gmail.com>
2024-12-20 09:17:03 +0530

    Connected to DB successfully, Entity init and added table of User

--- src/main/java/com/saad/security/user/User.java
+++ src/main/java/com/saad/security/user/User.java
@@ -1,5 +1,9 @@
 package com.saad.security.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +13,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String lastname;
